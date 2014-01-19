971d9b4adac1dbbaa81dbf8822fdc3eec2597136
araslanov_e <araslanov_e_i@list.ru>
2014-01-19 22:07:56 +0600

    Show directories and files

--- config.ru
+++ config.ru
@@ -3,6 +3,10 @@ require 'haml'
 require 'sass/plugin/rack'
 require './file_browser.rb'
 
+# Начальная директория
+set :root_path, '/home/araslanov'
+
+# Настройки Sass
 Sass::Plugin.options[:style] = :compressed
 use Sass::Plugin::Rack
 
--- file_browser.rb
+++ file_browser.rb
@@ -1,3 +1,32 @@
-get '/' do 
-  haml :index
+url = '/~'
+
+get '/' do
+  @url = url
+  haml :home
 end
+
+get "#{url}*?" do |dir| 
+
+  path = dir.to_s.strip
+  path << '/' unless path[-1, 1] == '/'
+
+  # Вернуться назад
+  @back = url + File.dirname(path)
+
+  # Содержимое текущей директории 
+  @directories, @files = [], []
+  current_path = settings.root_path + path
+  Dir.foreach(current_path) do |name|
+    full_path = current_path + '/' + name
+    if name != '.' && name != '..'
+      if File.directory?(full_path)
+        @directories << [url + path + name, name]
+      else
+        @files << name
+      end
+    end
+  end
+
+  haml :dir
+end
+
