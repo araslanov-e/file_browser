7d8033577966dc2e2d95a6780fd57a967f07acf2
araslanov_e <araslanov_e_i@list.ru>
2014-01-20 21:23:19 +0600

    Makeup

--- file_browser.rb
+++ file_browser.rb
@@ -7,25 +7,25 @@ end
 
 get "#{url}*?" do |dir| 
 
-  path = dir.to_s.strip
-  path << '/' unless path[-1, 1] == '/'
+  @path = dir.to_s.strip
+  @path << '/' unless @path[-1, 1] == '/'
 
   # Вернуться назад
-  @back = url + File.dirname(path)
+  @back = url + File.dirname(@path)
 
   # Содержимое текущей директории 
   @directories, @files = [], []
-  current_path = settings.root_path + path
+  current_path = settings.root_path + @path
   Dir.foreach(current_path) do |name|
     full_path = current_path + '/' + name
     if name != '.' && name != '..'
       if File.directory?(full_path)
-        @directories << [url + path + name, name]
+        @directories << [url + @path + name, name]
       else
         @files << name
       end
     end
-  end
+  end  
 
   haml :dir
 end
